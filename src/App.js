b40d4907ef103ff99d6dc049fb739f1bf3c738e0
Carlos Salazar <carlosiusalazar@gmail.com>
2020-07-27 20:44:22 +0900

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header'
+import Buttons from './Components/Buttons'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Buttons />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
